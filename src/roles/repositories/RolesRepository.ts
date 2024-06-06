cf4c43bf93289d5280e7bab492f54a1b124f23d8
Gui375 <guilherme.pinheiro.2c@gmail.com>
2024-06-06 12:38:39 -0300

    Reestruturação do repository

--- src/roles/repositories/RolesRepository.ts
+++ src/roles/repositories/RolesRepository.ts
@@ -1,14 +1,32 @@
 import { Role } from '@roles/http/routes/entities/Role'
+import { dataSource } from '@shared/typeorm'
+import { Repository } from 'typeorm'
 
 type CreateRoleDTO = {
   name: string
+  id: number
 }
+
+export type PaginateParams = {
+  //Criando o paginador
+  page: number
+  skip: number
+  take: number
+}
+
+export type RolesPaginateProperties = {
+  per_page: number
+  total: number
+  current_page: number
+  data: Role[]
+}
+
 export class RolesRepository {
-  private roles: Role[]
-  private static INSTANCE: RolesRepository //Implementação do singleton
+  private repository: Repository<Role> //Essa variavel vai manipular as informações da estrutura de dados role
+  private static INSTANCE: RolesRepository //Implementação do singleton | Tambem é a minha instancia
   //Static é uma variavel que não terá o valor alterado dentro do código
   private constructor() {
-    this.roles = []
+    this.repository = dataSource.getRepository(Role) //Atribuindo a minha propriedade repository o repositorio de um objeto Role
     //Quando instanciar a classe Role, ele fala que se iniciará como um array vazio
   }
 
@@ -22,24 +40,47 @@ export class RolesRepository {
   //Método usado para garantir que tenha apenas 1 instancia de RolesRepository dentro do código
   //If mostra que se a varivael INSTANCE (Onde guarda a informação da instancia do repositorio) for false, ele cria uma
 
-  create({ name }: CreateRoleDTO) {
-    const role = new Role()
-
-    Object.assign(role, {
-      name,
-      created_at: new Date(),
-    })
-    //Criando o objeto que será adicionado dentro do array roles
-
-    this.roles.push(role)
-    return role
+  async create({ name }: CreateRoleDTO): Promise<Role> {
+    //Como o método é assincrono ele tem sempre que retornar uma promessa
+    const role = this.repository.create({ name })
+    return this.repository.save(role)
   }
 
-  findByName(name: string): Role | undefined {
-    return this.roles.find(x => x.name == name)
+  async save(role: Role): Promise<Role> {
+    return this.repository.save(role)
   }
 
-  findAll(): Role[] {
-    return this.roles
+  async delete(role: Role): Promise<void> {
+    await this.repository.remove(role)
+  }
+  //Diferença do método create pro método save, é que no create estamos criando um novo objeto pra depois salvar. Já no Save, passamos uma propiedade como parametro e salvamos depois
+
+  async findByName(NameExe: string): Promise<Role | null> {
+    //TypeORM retorna null no lugar de undefinide
+    return await this.repository.findOneBy({ name: NameExe }) //Quando o nome do parametro do método é diferente do nome da coluna a ser buscada, temos que referenciar desta forma, porem quando é igual podemos passar somente o nome
+  }
+
+  async findById(id: string): Promise<Role | null> {
+    //TypeORM retorna null no lugar de undefinide
+    return await this.repository.findOneBy({ id: id }) //Quando o nome do parametro do método é diferente do nome da coluna a ser buscada, temos que referenciar desta forma, porem quando é igual podemos passar somente o nome
+  }
+
+  async findAll({
+    page,
+    skip,
+    take,
+  }: PaginateParams): Promise<RolesPaginateProperties> {
+    const [roles, count] = await this.repository
+      .createQueryBuilder()
+      .skip(skip)
+      .take(take) //Parametros é para o usuário dizer os valores que é pra pegar
+      .getManyAndCount()
+    const result = {
+      per_page: take,
+      total: count,
+      current_page: page,
+      data: roles,
+    }
+    return result
   }
 }
