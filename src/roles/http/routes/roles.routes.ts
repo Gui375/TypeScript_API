4757dbd6af7ecfe8e1d816425631bc35cab17f9b
Gui375 <guilherme.pinheiro.2c@gmail.com>
2024-06-28 12:34:56 -0300

    Adicionando validacao

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -4,28 +4,75 @@ import { createRolesController } from '@roles/UseCase/createRole'
 import { listRolesController } from '@roles/UseCase/listRole'
 import { showRolesController } from '@roles/UseCase/showRole'
 import { Router } from 'express'
+import { celebrate, Joi, Segments } from 'celebrate'
 
 const rolesRoutes = Router()
 
-rolesRoutes.post('/', (request, response) => {
-  return createRolesController.handle(request, response)
-})
+rolesRoutes.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+    }), //Validações do celebrate para a rota Post, validando que o nome tem que ser obrigatorio e uma string
+  }),
+  (request, response) => {
+    return createRolesController.handle(request, response)
+  },
+)
 
-rolesRoutes.get('/', (request, response) => {
-  return listRolesController.handle(request, response)
-})
+rolesRoutes.get(
+  '/',
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number(),
+      limit: Joi.number(),
+    }), //Validações do celebrate para a rota Post, validando que o nome tem que ser obrigatorio e uma string
+  }),
+  (request, response) => {
+    return listRolesController.handle(request, response)
+  },
+)
 
-rolesRoutes.get('/:id', (request, response) => {
-  //Rota com parametro
-  return showRolesController.handle(request, response)
-})
+rolesRoutes.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.string().uuid().required(),
+    }), //Validações do celebrate para a rota Post, validando que o nome tem que ser obrigatorio e uma string
+  }),
+  (request, response) => {
+    //Rota com parametro
+    return showRolesController.handle(request, response)
+  },
+)
 
-rolesRoutes.put('/:id', (request, response) => {
-  return updateRolesController.handle(request, response)
-})
+rolesRoutes.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+    }), //Validações do celebrate para a rota Post, validando que o nome tem que ser obrigatorio e uma string
+  }),
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.string().uuid().required(),
+    }), //Validações do celebrate para a rota Post, validando que o nome tem que ser obrigatorio e uma string
+  }),
+  (request, response) => {
+    return updateRolesController.handle(request, response)
+  },
+)
 
-rolesRoutes.delete('/:id', (request, response) => {
-  return deleteRolesController.handle(request, response)
-})
+rolesRoutes.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.string().uuid().required(),
+    }), //Validações do celebrate para a rota Post, validando que o nome tem que ser obrigatorio e uma string
+  }),
+  (request, response) => {
+    return deleteRolesController.handle(request, response)
+  },
+)
 
 export { rolesRoutes }
--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -3,19 +3,29 @@ import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
 import swaggerUi from 'swagger-ui-express'
 import { routes } from './routes'
-// import cors from 'cors'
+import cors from 'cors'
+import { errors } from 'celebrate'
 import { AppError } from '@shared/Errors/AppError'
 import swaggerFile from '../../swagger.json'
 
 const app = express()
 
-// app.use(cors)
+app.use(
+  cors({
+    origin: 'http://localhost:8081', // Permite apenas requisições do localhost:3000, ajuste conforme necessário
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
+    allowedHeaders: 'Content-Type,Authorization', // Cabeçalhos permitidos
+  }),
+)
 
 app.use(express.json()) //Falo pro express que ele vai usar json como retorno
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(routes) //Fala pro typeScript que as rotas serão executadas por esse objeto routes
+//O celebrate tem que ser passado obrigatoriamente após as rotas a posição é de extrema importancia
+app.use(errors()) //Detecta erros de validação e interrompe a execução das rotas
 
 app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
       return response.status(error.statusCode).json({
