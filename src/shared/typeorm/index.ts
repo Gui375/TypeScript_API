f53405cd630acba82ecd1f6b2be9f399b568b46f
Gui375 <guilherme.pinheiro.2c@gmail.com>
2024-07-11 17:06:46 -0300

    Criacao usersTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -2,6 +2,7 @@
 import { DataSource } from 'typeorm'
 import { CreateRolesTable1717677183833 } from './migrations/1717677183833-CreateRolesTable'
 import { Role } from '@roles/http/routes/entities/Role'
+import { CreateUsersTable1720727564124 } from './migrations/1720727564124-CreateUsersTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
@@ -11,7 +12,7 @@ export const dataSource = new DataSource({
   // password: 'test',
   database: './db.sqlite',
   entities: [Role], //Definindo as entidades
-  migrations: [CreateRolesTable1717677183833], //Definindo as migracoes
+  migrations: [CreateRolesTable1717677183833, CreateUsersTable1720727564124], //Definindo as migracoes   Usamos esse campo apenas para criar a tabela no banco de dados
 })
 
 //Comando pra executar as migration npm run typeorm -- -d ./src/shared/typeorm/index.ts  migration:run
